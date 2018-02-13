ae4dadbc437afb7770891d67e7c535053644168e
magicpine <c00223037@itcarlow.ie>
2018-02-13 15:04:22 +0000

    Fixed the get_post_count method in my models.py and everything else associated with it

--- myproject/boards/models.py
+++ myproject/boards/models.py
@@ -1,18 +1,26 @@
-from django.db import models
-from django.contrib.auth.models import User
-from django.utils.text import Truncator
-from django.utils.html import mark_safe
-from markdown import markdown
 import math
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils.html import mark_safe
+from django.utils.text import Truncator
+
+from markdown import markdown
 
 
 class Board(models.Model):
     name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
-        
+
+    def get_posts_count(self):
+        return Post.objects.filter(topic__board=self).count()
+
+    def get_last_post(self):
+        return Post.objects.filter(topic__board=self).order_by('-created_at').first()
+
 
 class Topic(models.Model):
     subject = models.CharField(max_length=255)
@@ -20,10 +28,10 @@ class Topic(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='topics')
     starter = models.ForeignKey(User, on_delete=models.CASCADE, related_name='topics')
     views = models.PositiveIntegerField(default=0)
-    
+
     def __str__(self):
         return self.subject
-        
+
     def get_page_count(self):
         count = self.posts.count()
         pages = count / 20
@@ -39,10 +47,9 @@ class Topic(models.Model):
         if self.has_many_pages(count):
             return range(1, 5)
         return range(1, count + 1)
-        
+
     def get_last_ten_posts(self):
         return self.posts.order_by('-created_at')[:10]
-
 
 
 class Post(models.Model):
@@ -52,12 +59,10 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='+')
-    
-    def get_message_as_markdown(self):
-        return mark_safe(markdown(self.message, safe_mode='escape'))
-        
-        
-        
+
     def __str__(self):
         truncated_message = Truncator(self.message)
         return truncated_message.chars(30)
+
+    def get_message_as_markdown(self):
+        return mark_safe(markdown(self.message, safe_mode='escape'))
